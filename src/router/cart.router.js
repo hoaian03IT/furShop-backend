d71b1903c445596be7595017883054867eaabbe7
NguyenTHanhAn853202 <nguyenthanhanqt@gmail.com>
2024-02-20 16:50:57 +0700

    cart function

---
+++ src/router/cart.router.js
@@ -0,0 +1,5 @@
+const routes = require('express').Router();
+
+routes.post('/them-vao-gio-hang',)
+
+module.exports = routes
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 const getImage = require('./getImage.route')
 const Product = require('./product.router')
-
+const Cart = require('./cart.router')
 function router(app) {
     app.use('/hinh-anh',getImage)
-    app.use('/tai-khoan')
+    // app.use('/tai-khoan')
+    app.use('/gio-hang',Cart)
     app.use('/',Product)
 }
 
