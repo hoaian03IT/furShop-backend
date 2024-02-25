bfdd210ebd1c493b0a72091cd0be03cfc5da3d92
hoaian03 <dangtranhoaian8603@gmail.com>
2024-02-25 12:19:25 +0700

    chinh sua query product

--- src/app/model/cart.js
+++ src/app/model/cart.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const mongooseDelete = require('mongoose-delete')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const mongooseDelete = require("mongoose-delete");
 
-const CartSchema = new Schema({
-   amount:{type:Number,default:0},
-   customerId:{type:mongoose.Schema.Types.ObjectId,ref:'Account'},
-   productId:{type:mongoose.Schema.Types.ObjectId,ref:'Product'},
-},{timestamps:true})
+const CartSchema = new Schema(
+    {
+        amount: { type: Number, default: 0 },
+        customerId: { type: mongoose.Schema.ObjectId, ref: "Account" },
+        productId: { type: mongoose.Schema.ObjectId, ref: "Product" },
+    },
+    { timestamps: true }
+);
 
-CartSchema.plugin(mongooseDelete,{deletedAt:true,deletedBy:true})
+CartSchema.plugin(mongooseDelete, { deletedAt: true, deletedBy: true });
 
-module.exports = mongoose.model('Cart',CartSchema)
+module.exports = mongoose.model("Cart", CartSchema);
--- src/app/model/order.js
+++ src/app/model/order.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const mongooseDelete = require('mongoose-delete')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const mongooseDelete = require("mongoose-delete");
 
-const OrderSchema = new Schema({
-   amount:{type:Number,default:0},
-   paymentType:{type:Number,default:0,enums:[0,1]}, // 0 la thanh toan khi nhan hang, 1 la thanh toan truc tuyen
-   customerId:{type:mongoose.Schema.Types.ObjectId,ref:'Account'},
-   productId:{type:mongoose.Schema.Types.ObjectId,ref:'Product'},
-},{timestamps:true})
+const OrderSchema = new Schema(
+    {
+        amount: { type: Number, default: 0 },
+        paymentType: { type: Number, default: 0, enums: [0, 1] }, // 0 la thanh toan khi nhan hang, 1 la thanh toan truc tuyen
+        customerId: { type: mongoose.Schema.ObjectId, ref: "Account" },
+        productId: { type: mongoose.Schema.ObjectId, ref: "Product" },
+    },
+    { timestamps: true }
+);
 
-OrderSchema.plugin(mongooseDelete,{deletedAt:true,deletedBy:true})
+OrderSchema.plugin(mongooseDelete, { deletedAt: true, deletedBy: true });
 
-module.exports = mongoose.model('Order',OrderSchema)
+module.exports = mongoose.model("Order", OrderSchema);
